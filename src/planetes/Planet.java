04ba456bfe6846fe6ddd6d42ba2f37c9095ed7d3
David Ibarzabal <david_soc@hotmail.com>
2018-04-26 14:59:33 -0400

    v1

---
+++ src/planetes/Planet.java
@@ -0,0 +1,61 @@
+package planetes;
+
+/**
+ * Created by ibada1731710 on 2018-04-26.
+ */
+public class Planet {
+    private String nom;
+    private int nbPlut;
+    private int nbThul;
+    private int nbGad;
+    private int nbTer;
+    private int nbNep;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNbPlut() {
+        return nbPlut;
+    }
+
+    public void setNbPlut(int nbPlut) {
+        this.nbPlut = nbPlut;
+    }
+
+    public int getNbThul() {
+        return nbThul;
+    }
+
+    public void setNbThul(int nbThul) {
+        this.nbThul = nbThul;
+    }
+
+    public int getNbGad() {
+        return nbGad;
+    }
+
+    public void setNbGad(int nbGad) {
+        this.nbGad = nbGad;
+    }
+
+    public int getNbTer() {
+        return nbTer;
+    }
+
+    public void setNbTer(int nbTer) {
+        this.nbTer = nbTer;
+    }
+
+    public int getNbNep() {
+        return nbNep;
+    }
+
+    public void setNbNep(int nbNep) {
+        this.nbNep = nbNep;
+    }
+}
